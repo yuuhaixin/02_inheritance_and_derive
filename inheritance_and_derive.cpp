a3c0d42e9ee9a08982cab60b79a5ac8a84512fb5
yuhaixin <yu_haixin@outlook.com>
2026-01-08 13:12:04 +0800

    实现类的继承及基础操作

---
+++ inheritance_and_derive.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+class People
+{
+protected:
+    string name;
+    int age;
+
+public:
+    People();
+    People(string name, int age);
+    ~People();
+
+    void display() const;
+};
+
+People::People() : name("none"), age(0)
+{
+    cout << "执行People类默认构造函数..." << name << endl;
+}
+
+People::People(string name, int age) : name(name), age(age)
+{
+    cout << "执行People类含参构造函数..." << name << endl;
+}
+
+People::~People()
+{
+    cout << "执行People类析构函数..." << name << endl;
+}
+
+void People::display() const
+{
+    cout << "name: " << name << ", age: " << age << endl;
+}
+
+class Teacher : public People
+{
+private:
+    string working_place;
+
+public:
+    Teacher(string name, int age, string w_p);
+    ~Teacher();
+    void teach();
+};
+
+Teacher::Teacher(string name, int age, string w_p) : People(name, age), working_place(w_p)
+{
+    cout << "执行Teacher类含参构造函数..." << name << endl;
+}
+
+Teacher::~Teacher()
+{
+    cout << "执行Teacher类析构函数..." << name << endl;
+}
+
+void Teacher::teach()
+{
+    cout << name << " works in " << working_place << endl;
+}
+
+int main()
+{
+    People ZhangSan;
+    People *LiSi = new People("LiSi", 15);
+    LiSi->display();
+
+    Teacher LiMing = Teacher("LiMing", 30, "School");
+    LiMing.display();
+    LiMing.teach();
+
+    delete LiSi;
+    return 0;
+}
